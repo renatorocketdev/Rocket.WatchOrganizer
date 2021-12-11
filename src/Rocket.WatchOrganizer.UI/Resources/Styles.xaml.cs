08ef53ec092d006de77e04ff3b1543f595e33b9e
Renato Alves <renatoalves.silvaal@gmail.com>
2021-12-11 16:11:52 -0300

    Adding Styles xaml and a exemplo to work it

---
+++ src/Rocket.WatchOrganizer.UI/Resources/Styles.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace Rocket.WatchOrganizer.UI.Resources
+{
+    public partial class Styles : ResourceDictionary
+    {
+        public Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}
