1f43c590e7b353b44b336a0c6e4e83acc0cbfdca
Caio Sales <caiofernandosales@outlook.com>
2021-12-11 15:00:23 -0300

    added material icon font and nunito font

--- src/Rocket.WatchOrganizer.UI/App.xaml.cs
+++ src/Rocket.WatchOrganizer.UI/App.xaml.cs
@@ -1,8 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+//Declaramos aqui o nome do arquivo da fonte e o "Alias" que vamos utilizar para chamar a fonte
+
+//MATERIAL DESIGN FONTS
+[assembly: ExportFont("MaterialIconsOutlined-Regular.otf", Alias = "MaterialIconsOutlined")]
+[assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = "MaterialIconsRegular")]
+[assembly: ExportFont("MaterialIconsRound-Regular.otf", Alias = "MaterialIconsRound")]
+[assembly: ExportFont("MaterialIconsSharp-Regular.otf", Alias = "MaterialIconsSharp")]
+[assembly: ExportFont("MaterialIconsTwoTone-Regular.otf", Alias = "MaterialIconsTwoTone")]
+
+//INTERFACE FONTS
+[assembly: ExportFont("Nunito-Black.ttf", Alias = "NunitoBlack")]
+[assembly: ExportFont("Nunito-Bold.ttf", Alias = "NunitoBold")]
+[assembly: ExportFont("Nunito-ExtraBold.ttf", Alias = "NunitoExtraBold")]
+[assembly: ExportFont("Nunito-ExtraLight.ttf", Alias = "NunitoExtraLight")]
+[assembly: ExportFont("Nunito-Italic.ttf", Alias = "NunitoItalic")]
+[assembly: ExportFont("Nunito-Light.ttf", Alias = "NunitoLight")]
+[assembly: ExportFont("Nunito-Medium.ttf", Alias = "NunitoMedium")]
+[assembly: ExportFont("Nunito-Regular.ttf", Alias = "NunitoRegular")]
+[assembly: ExportFont("Nunito-SemiBold.ttf", Alias = "NunitoSemiBold")]
+
 
 namespace Rocket.WatchOrganizer.UI
 {
