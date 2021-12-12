ccd651b6751b2b039fbd0b8512013643da01deda
Caio Sales <caiofernandosales@outlook.com>
2021-12-12 11:14:01 -0300

    ui update 0.1

--- src/Rocket.WatchOrganizer.Core/ViewModels/Home/HomeViewModel.cs
+++ src/Rocket.WatchOrganizer.Core/ViewModels/Home/HomeViewModel.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.Navigation;
+using Rocket.WatchOrganizer.Core.Models;
 using Rocket.WatchOrganizer.Core.Service;
 using Rocket.WatchOrganizer.Core.ViewModels.WatchList;
 
@@ -20,6 +23,74 @@ namespace Rocket.WatchOrganizer.Core.ViewModels.Home
                 _service = new SerieService();
             }
         }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+            GetWatchListAsync();
+        }
+
+        private void GetWatchListAsync()
+        {
+            Series = new List<Serie>
+            {
+                new Serie
+                {
+                    Banner = "https://studiosol-a.akamaihd.net/uploadfile/letras/playlists/3/1/1/c/311c0dc086b14fd29c2fa085771fe61e.jpg",
+                },
+                new Serie
+                {
+                    Banner = "https://manualdosgames.com/wp-content/uploads/2020/10/The-Witcher.png",
+                },
+                new Serie
+                {
+                    Banner = "https://presleyson.com.br/wp-content/uploads/2019/02/mr-robot-1200x630-min.jpg"
+                }
+            };
+
+            Animes = new List<Serie>
+            {
+                new Serie
+                {
+                    Banner = "https://criticalhits.com.br/wp-content/uploads/2021/11/Luffy_and_His_Crew.png",
+                },
+                new Serie
+                {
+                    Banner = "https://cdn.pensador.com/img/authors/na/ru/naruto-l.jpg",
+                },
+                new Serie
+                {
+                    Banner = "https://www.einerd.com.br/wp-content/uploads/2018/09/bleach-sa%C3%BAde-tite-kubo-capa.jpg"
+                }
+            };
+        }
+
+
+        #region Properties
+
+        private List<Serie> _series;
+        public List<Serie> Series
+        {
+            get => _series;
+            set
+            {
+                _series = value;
+                RaisePropertyChanged(() => Series);
+            }
+        }
+
+        private List<Serie> _animes;
+        public List<Serie> Animes
+        {
+            get => _animes;
+            set
+            {
+                _animes = value;
+                RaisePropertyChanged(() => Animes);
+            }
+        }
+
+        #endregion
 
         public IMvxAsyncCommand ShowSeriesCommand => new MvxAsyncCommand(async () => await ShowSeriesAsync());
 
